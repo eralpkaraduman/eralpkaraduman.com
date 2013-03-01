7f2e53cc978b55517194f8eb95f7b740d1d1f1ce
Eralp Karaduman <eralpkaraduman@gmail.com>
2013-03-01 15:51:20 +0200

    bebilip

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('<a href="http://twitter.com/eralpkaraduman">eralp</a>');
+  response.send('<head><meta http-equiv="Content-type" content="text/html; charset=utf-8"></head><body><p>bazen olan şeyler her zaman olmayabilir</p><br/><a href="http://twitter.com/eralpkaraduman">eralp</a></body>');
 });
 
 var port = process.env.PORT || 5000;
