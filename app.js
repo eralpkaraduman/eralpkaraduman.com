5396b4fe3ba9865fc37c871d624532f8bd175a37
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-06-29 14:12:29 +0300

    fixed typo & changed projects object format

--- app.js
+++ app.js
@@ -55,22 +55,19 @@ var blog = '<p><a href="http://biseyyapiyorum.tumblr.com">things i do</a></p>'
 			"C# (Unity3D)",
 			"Java (Android)",
 			"Blender 3D",
-			"Accpetable Art Skills",
 			"JavaScript",
-			"Node.js"	
+			"Node.js"
 		],
 		notableProjects:[
-			{"LD48 - Rockmorse":"http://superdamage.itch.io/rockmorse"}
+			{
+				"name":"LD48 - Rockmorse",
+				"url":"http://superdamage.itch.io/rockmorse"
+			}
 		]
 	};
 
 	response.send('<html><head><meta http-equiv="Content-type" content="text/html; charset=utf-8"></head><body><pre>' + JSON.stringify(obj, undefined, 4) + '</pre>'+donate+'</body></html>');
 
-	
-
-	//response.send(JSON.stringify(obj, undefined, 4));
-
-  //response.send('<head><meta http-equiv="Content-type" content="text/html; charset=utf-8"></head><body><h1>This Is My Web Site</h1><h3>I\'m Eralp Karaduman, half of <a href="http://superdamage.com">Super Damage Games</a></h3>'+donate+'<br/>'+blog+'<a href="http://twitter.com/eralpkaraduman">tweets</a></body>');
 });
 
 var port = process.env.PORT || 4000;
