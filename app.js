3039e3276b6b1b7d6602f8ee12d95f82f6e51a8f
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-06-29 14:16:23 +0300

    added python to skills

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ var blog = '<p><a href="http://biseyyapiyorum.tumblr.com">things i do</a></p>'
 			"C/C++",
 			"C# (Unity3D)",
 			"Java (Android)",
+			"Python",
 			"Blender 3D",
 			"Accepetable Art Skills",
 			"JavaScript",
