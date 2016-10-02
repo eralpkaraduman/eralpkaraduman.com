09e2efd3d684e843ba3846281216c681dfae3e21
Eralp Karaduman <eralpkaraduman@gmail.com>
2016-10-03 01:16:42 +0200

    improved api response

--- main.go
+++ main.go
@@ -8,6 +8,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Person struct {
+	Id string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+	Email string `json:"email"`
+	Occupation string `json:"occupation"`
+	Titles []Title `json:"titles"`
+	Skills []string `json:"skills"`
+	Projects []Project `json:"projects"`
+}
+
+type Title struct {
+	Name string `json:"name"`
+	Url string `json:"url"`
+}
+
+type Project struct {
+	Name string `json:"name"`
+	Url string `json:"url"`
+	Description string `json:"description"`
+	Roles []string `json:"roles"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -25,9 +48,45 @@ func main() {
 	})
 
 	router.GET("/people/eralp", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+		eralp := Person{
+			Id: "0",
+			FirstName: "Eralp",
+			LastName: "Karaduman",
+			Email: "eralpkaraduman@gmail.com",
+			Occupation: "Full Stack Mobile Software Developer",
+			Titles: []Title{
+				Title{Name: "iOS Developer At DealDash", Url: "http://dealdash.com"},
+				Title{Name: "Co-Founder At CupsOn.Me", Url: "http://cupson.me"},
+				Title{Name: "Half Of Super Damage Games", Url: "http://superdamage.com"},
+			},
+			Skills: []string{
+				"Product Development",
+				"Game Design",
+				"Swift (iOS)",
+				"Objective-C (iOS)",
+				"Go",
+				"Java (Android)",
+				"Node.js",
+				"PhP (Symfony)",
+				"C# (Unity3D)",
+				"Blender 3D",
+				"Accepetable Art Skills",
+			},
+			Projects: []Project{
+				Project{
+					Name: "Rockmorse",
+					Url: "http://superdamage.itch.io/rockmorse",
+					Roles: []string {
+						"Unity3D C# Programming",
+						"Game Design",
+					},
+					Description: "Desktop/browser video game made with Unity3D for Ludum Dare game making competition",
+				},
+			},
+		}
+
+		c.JSON(200, eralp)
 	})
 
 	router.Run(":" + port)
