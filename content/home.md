bd7d0603d2124d83fde3f24108a403bca02e0d7f
Eralp Karaduman <eralpkaraduman@gmail.com>
2016-10-12 04:16:21 +0300

    mvp

---
+++ content/home.md
@@ -0,0 +1,16 @@
+# Eralp Karaduman
+
+![eralp](https://gravatar.com/avatar/0612a1a35de3e5d07bb2d5b58a254005?s=200)
+
+I'm a **full stack mobile software developer** based in Helsinki, Finland.  
+I build mostly iOS apps along with their  backend and Android counterparts.  
+When I'm not working on apps, I like making games at our own [Super Damage Games](http://superdamage.com)
+
+**I'm available for hire!**   
+I have recently shipped a product,  
+and now looking for the next exciting project to work on.
+
+Best ways to contact me would be;  
+**Email:** [eralp@eralpkaraduman.com](mailto://eralp@eralpkaraduman.com)  
+**Twitter:** [@eralpkaraduman](twitter.com/eralpkaraduman)  
+*([PGP Key](https://keybase.io/eralp/key.asc), FingerPrint: 0350 3E22 3153 4F9F CB96 9513 CF03 F1AB 4549 DF7F)*
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	//"fmt"
 	"log"
 	"net/http"
 	"os"
+	"io/ioutil"
+	"path"
+	"path/filepath"
+	"html/template"
 
 	"github.com/gin-gonic/gin"
+	"github.com/russross/blackfriday"
 )
 
 func main() {
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -21,8 +28,22 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+
+		c.HTML(http.StatusOK, "base", gin.H{
+			"content": markDownContentHTML("home.md"),
+		})
+
 	})
 
 	router.Run(":" + port)
+}
+
+func markDownContentHTML(mdPath string) template.HTML {
+	relativeContentPath := path.Join("content", mdPath)
+	absoluteContentPath, _ := filepath.Abs(relativeContentPath)
+	contentData, _ := ioutil.ReadFile(absoluteContentPath)
+	//contentString := string(contentData)
+	htmlContent := blackfriday.MarkdownCommon(contentData)
+	htmlString := string(htmlContent)
+	return template.HTML(htmlString)
 }
