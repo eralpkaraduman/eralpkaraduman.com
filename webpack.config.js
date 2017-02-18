3874f9f1b0a65a1187d704cd8016a3c8aeab549b
Eralp Karaduman <eralpkaraduman@gmail.com>
2017-02-18 04:53:07 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+const packageConfig = require('./package');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    index: "./index.js"
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  devServer:{
+    contentBase: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'jsxhint-loader'
+      },
+      {
+		    test: /\.js?$/,
+		    exclude: /node_modules/,
+		    loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+		  }
+		]
+  },
+  plugins: [new HtmlPlugin({
+        title: packageConfig.name,
+        template: 'index.ejs',
+        inject: 'body'
+    })],
+  resolve: {
+    extensions: ['.js', '.es6']
+  },
+}
