502a1ad04c164f8743c74c8f1882a02c7afe32c7
Eralp Karaduman <eralpkaraduman@gmail.com>
2017-02-20 01:52:54 +0200

    use hi res image

--- src/profilePicture.js
+++ src/profilePicture.js
@@ -3,6 +3,10 @@ import { Image } from 'react-bootstrap';
 
 const imageStyle = {
   cursor: 'normal',
+  height: 'auto',
+  width: 'auto',
+  maxWidth: 140,
+  maxHeight: 'auto',
 };
 
 class ProfilePicture extends Component {
@@ -10,7 +14,7 @@ class ProfilePicture extends Component {
   render() {
     return (
       <Image
-        src="https://gravatar.com/avatar/0612a1a35de3e5d07bb2d5b58a254005?s=140"
+        src="https://gravatar.com/avatar/0612a1a35de3e5d07bb2d5b58a254005?s=800"
         circle
         thumbnail
         style={imageStyle}
