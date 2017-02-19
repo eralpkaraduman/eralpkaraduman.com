b4d3c681f1df551ed31d603b78e82f20dc988467
Eralp Karaduman <eralpkaraduman@gmail.com>
2017-02-19 02:48:05 +0200

    added profile picture

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import ProfilePicture from './profilePicture';
+
+const containerStyle = {
+  margin: 20,
+};
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <Button>sidik</Button>
+      <div className="row center-xs" style={containerStyle}>
+        <div className="col-xs-6">
+          <div className="box">
+            <ProfilePicture />
+          </div>
+        </div>
       </div>
     );
   }
