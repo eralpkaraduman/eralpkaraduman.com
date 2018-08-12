770101ad297fd3f54f012f2420f3b9b12e96f27d
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-08-13 00:54:22 +0300

    Fixed layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import './index.css'
 import 'flexboxgrid';
+import './index.css'
 
 const Layout = ({ children, data }) => (
   <div className="row center-xs">
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
     <div className="col-xs-12 col-sm-8 col-md-6">
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
