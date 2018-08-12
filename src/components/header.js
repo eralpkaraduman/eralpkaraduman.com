6af52902a4b4b2151f912197144b99b00270dcfa
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-08-13 00:38:16 +0300

    Implemented header layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components';
+import logoImage from './logo.png';
+
+const HeaderContainer = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 1.45rem;
+`;
+
+const Logo = styled.img`
+  margin: 0px;
+  padding: 0px;
+  height: auto;
+  width: auto;
+  max-width: 140px;
+  max-height: auto;
+`;
+
+const SiteTitle = styled.h1`
+  margin: 0px;
+  font-weight: bold;
+  font-size: 28px;
+`;
+
+const StyledLink = styled(Link)`
+  color: rgba(0,0,0,0.8);
+  text-decoration: none;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
+  <HeaderContainer>
+    <div className='row center-xs'>
+      <div className="col-xs">
+          <Logo src={logoImage} alt="Logo" />
+          <SiteTitle>
+            <StyledLink to="/">{siteTitle}</StyledLink>
+          </SiteTitle>
+      </div>
+    </div>
+    {/* <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
@@ -26,8 +56,8 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </div>
+    </div> */}
+  </HeaderContainer>
 )
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,9 +4,10 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import 'flexboxgrid';
 
 const Layout = ({ children, data }) => (
-  <div>
+  <div className="row center-xs">
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
@@ -14,16 +15,18 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+    <div className="col-xs-12 col-sm-8 col-md-6">
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
     </div>
   </div>
 )
